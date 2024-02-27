d75ef734bdf2dbdfc7e3d27fd74971ccb3c7219e
shraddha <shraddha9415@gmail.com>
2024-02-27 23:22:36 +0530

    first commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+const data=[
+    {
+        id:'1',
+        question:"Who are you?",
+        answer:"I am Shraddha"
+    },
+    {
+        id:'2',
+        question:"What do you do?",
+        answer:"I am a computer science undergraduate"
+    },
+    {
+        id:'3',
+        question:"Do you have a good job yet?",
+        answer:"Nope"
+    },
+    {
+        id:'4',
+        question:"What will you do?",
+        answer:":)"
+    }
+
+];
+
+export default data;
