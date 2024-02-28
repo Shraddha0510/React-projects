f4681dc1d3906141ee2bc5e41925995e63549b7f
shraddha <shraddha9415@gmail.com>
2024-02-28 22:51:03 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Accordian from './components/accordian';
+import StarRating from './components/star-rating';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Accordian></Accordian>
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
