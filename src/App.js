73abed50b09acd7a20d63833fd656fe2e78f419f
shraddha <shraddha9415@gmail.com>
2024-03-04 23:10:48 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Accordian from './components/accordian';
 import StarRating from './components/star-rating';
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import './App.css';
 
 function App() {
@@ -7,6 +9,13 @@ function App() {
     <div className="App">
       <Accordian></Accordian>
       <StarRating noOfStars={10}/>
+          {/* Image slider component */}
+        <ImageSlider
+          url={"https://picsum.photos/v2/list"}
+          page={"1"}
+          limit={"10"}
+        /> 
+      <LoadMoreData/>
     </div>
   );
 }
